3466e25fa5516806e360b29b76322ba67addfbca
Gonzalo Uribe <guribemontero@gmail.com>
2018-03-27 23:42:40 -0300

    Creates a bridge between isometric coordinates and screen points

---
+++ Assets/Controller/IsometricTransformer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace com.gStudios.isometric.controller {
+
+	public static class IsometricTransformer {
+		// Based on the algorithms found on http://clintbellanger.net/articles/isometric_math/
+
+		public const float TILE_WIDTH = 1;
+		public const float TILE_HEIGHT = 0.5f;
+
+		public const float TILE_WIDTH_HALF = 0.5f;
+		public const float TILE_HEIGHT_HALF = 0.25f;
+
+		/// <summary>
+		/// Converts isometric tile coordinates to a screen position.
+		/// </summary>
+		/// <returns>The screen position.</returns>
+		/// <param name="x">The x isometric coordinate.</param>
+		/// <param name="y">The y isometric coordinate.</param>
+		public static Vector2 CoordToScreen(int x, int y) {
+			return new Vector2((x - y) * TILE_WIDTH_HALF, (x + y) * TILE_HEIGHT_HALF);
+		}
+
+	}
+
+}
