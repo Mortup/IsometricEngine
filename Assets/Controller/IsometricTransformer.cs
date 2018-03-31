b8a94320f408c4c2e4e2e44f03f6421a63469dbd
Gonzalo Uribe <guribemontero@gmail.com>
2018-03-31 16:56:55 -0300

    Adds reverse transform and takes care of top-left pivot

--- Assets/Controller/IsometricTransformer.cs
+++ Assets/Controller/IsometricTransformer.cs
@@ -15,13 +15,31 @@ namespace com.gStudios.isometric.controller {
 		public const float TILE_HEIGHT_HALF = 0.25f;
 
 		/// <summary>
-		/// Converts isometric tile coordinates to a screen position.
+		/// Converts isometric tile coordinates to a world position.
 		/// </summary>
-		/// <returns>The screen position.</returns>
+		/// <returns>The world position.</returns>
 		/// <param name="x">The x isometric coordinate.</param>
 		/// <param name="y">The y isometric coordinate.</param>
-		public static Vector2 CoordToScreen(int x, int y) {
-			return new Vector2((y - x) * TILE_WIDTH_HALF, -(x + y) * TILE_HEIGHT_HALF);
+		public static Vector2 CoordToWorld(int x, int y) {
+			Vector2 world = new Vector2 ((y - x) * TILE_WIDTH_HALF, -(x + y) * TILE_HEIGHT_HALF);
+			Vector2 offset = new Vector2 (TILE_WIDTH_HALF, -TILE_HEIGHT_HALF);
+			return world - offset;
+		}
+
+		/// <summary>
+		/// Converts world position to isometric tile coordinates.
+		/// </summary>
+		/// <returns>The tile coordinate coordinate.</returns>
+		/// <param name="screen">The screen position.</param>
+		public static Vector2Int WorldToCoord(Vector2 world) {
+			world.x = -world.x;
+			world.y = -world.y;
+
+			Vector2Int map = Vector2Int.zero;
+			map.x = Mathf.RoundToInt((world.x / TILE_WIDTH_HALF + world.y / TILE_HEIGHT_HALF) /2);
+			map.y = Mathf.RoundToInt((world.y / TILE_HEIGHT_HALF - (world.x / TILE_WIDTH_HALF)) / 2);
+
+			return map;
 		}
 
 	}
