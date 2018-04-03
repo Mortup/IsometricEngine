354f45ebbd93500174cc065fda461f3765572f0f
Gonzalo Uribe <guribemontero@gmail.com>
2018-04-03 19:57:44 -0300

    Adds comments for the interface documentation"

--- Assets/Controller/Cursor/CursorMode.cs
+++ Assets/Controller/Cursor/CursorMode.cs
@@ -11,14 +11,39 @@ namespace com.gStudios.isometric.controller.cursor {
 	/// </summary>
 	public interface CursorMode {
 
+		/// <summary>
+		/// Should be called when the user starts a mouse click.
+		/// </summary>
+		/// <param name="mousePosition">Mouse position.</param>
 		void ClickStart (Vector2 mousePosition);
 
+		/// <summary>
+		/// Should be called when the user finishes a mouse click.
+		/// </summary>
+		/// <returns>The end.</returns>
+		/// <param name="mousePosition">Mouse position.</param>
 		CursorCommand ClickEnd(Vector2 mousePosition);
 
+		/// <summary>
+		/// Called when the mode is activated.
+		/// </summary>
+		void TurnOn();
+
+		/// <summary>
+		/// Should be called when the mode is deactivated.
+		/// </summary>
+		void TurnOff();
+
+		/// <summary>
+		/// Updates the cursors on scene.
+		/// </summary>
+		void UpdateCursors();
+
+		/// <summary>
+		/// Sets the index of the object the mode will be editing.
+		/// </summary>
+		/// <param name="index">Index.</param>
 		void SetSelectedIndex (int index);
-
-		void DestroyCursors();
-
 	}
 
 }
