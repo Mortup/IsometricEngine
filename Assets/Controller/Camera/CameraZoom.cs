00fc5702456cd817fe06611f760e9be70267e5d9
Gonzalo Uribe <guribemontero@gmail.com>
2018-04-27 14:49:50 -0300

    Adds zoom to camera

---
+++ Assets/Controller/Camera/CameraZoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.gStudios.isometric.controller.camera {
+
+	public class CameraZoom : MonoBehaviour {
+
+		Camera cam;
+
+		void Awake() {
+			cam = GetComponent<Camera> ();
+		}
+
+		void Start() {
+			cam.orthographicSize = 4f;
+		}
+
+		void Update() {
+			float newSize = cam.orthographicSize * Mathf.Pow(2, Input.mouseScrollDelta.y);
+			newSize = Mathf.Max (1f, newSize);
+			newSize = Mathf.Min (8f, newSize);
+
+			cam.orthographicSize = newSize;
+		}
+
+		void OnGUI() {
+			GUILayout.Label (cam.orthographicSize.ToString ());
+		}
+	}
+
+}
