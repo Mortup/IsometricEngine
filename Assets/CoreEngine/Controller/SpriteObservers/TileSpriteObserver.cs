5bf1c0f329b493d21868097af44139134a2cb70e
Gonzalo Uribe <guribemontero@gmail.com>
2018-09-09 17:38:01 -0300

    Removed unused imports

--- Assets/CoreEngine/Controller/SpriteObservers/TileSpriteObserver.cs
+++ Assets/CoreEngine/Controller/SpriteObservers/TileSpriteObserver.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 using UnityEngine;
 
-using com.gStudios.isometric.controller;
 using com.gStudios.isometric.controller.isometricTransform;
 using com.gStudios.isometric.controller.data;
 
