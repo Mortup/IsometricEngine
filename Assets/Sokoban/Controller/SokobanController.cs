45014f1c153ad22daf642e7ebc01690fec59efbc
Mortup <g.uribemontero@gmail.com>
2018-09-27 14:45:30 -0300

    On end loads next level

--- Assets/Sokoban/Controller/SokobanController.cs
+++ Assets/Sokoban/Controller/SokobanController.cs
@@ -9,20 +9,26 @@ using com.gStudios.isometric.model.characters;
 using com.gStudios.isometric.model.world;
 
 using com.gStudios.sokoban.model.saving;
+using com.gStudios.isometric.model.world.tile;
 
 namespace com.gStudios.sokoban.controller {
 
     public class SokobanController : MonoBehaviour, ILevelController {
 
         private CoreLevelController coreLevelController;
+        private Level level;
 
+        private int currentLevelIndex = 1;
+               
         public void Init(CoreLevelController clc) {
             coreLevelController = clc;
 
-            coreLevelController.LoadLevel(new SokobanLevelSerializer("level2"));
+            LoadNextLevel();
         }
 
         public void OnLevelInit(Level level) {
+            this.level = level;
+
             List<ICharacter> chars = level.GetCharacters();
 
             if (chars.Count > 1)
@@ -33,6 +39,37 @@ namespace com.gStudios.sokoban.controller {
             cc.Init(chars[0]);
             FourDirectionsSpriteCC sprCC = playerController.AddComponent<FourDirectionsSpriteCC>();
             sprCC.Init(chars[0], "Player");
+        }
+
+        public void Update() {
+            if (HasWon()) {
+                Debug.Log("You Win!");
+                LoadNextLevel();
+            }
+
+        }
+
+        private bool HasWon() {
+            bool hasWon = true;
+
+            for (int x = 0; x < level.Width; x++) {
+                for (int y = 0; y < level.Height; y++) {
+                    ITile currentTile = level.GetTileAt(x, y);
+                    if (currentTile.Type == 2) {
+                        if (currentTile.GetPlacedFurniture().GetTag() != "Box") {
+                            hasWon = false;
+                        }
+                    }
+                }
+            }
+
+            return hasWon;
+
+        }
+
+        private void LoadNextLevel() {
+            coreLevelController.LoadLevel(new SokobanLevelSerializer("level" + currentLevelIndex.ToString()));
+            currentLevelIndex++;
         }
 
     }
