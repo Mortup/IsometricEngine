a56a07bed2399a6b29f39b0ca818f30d83082207
Gonzalo Uribe <guribemontero@gmail.com>
2018-10-23 10:53:35 -0300

    Added walking sound effect

---
+++ Assets/Sokoban/Controller/SoundChooser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundChooser : MonoBehaviour {
+
+    [SerializeField] AudioClip movement;
+
+    AudioSource source;
+
+    private void Awake() {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void Play(string soundName) {
+        source.pitch = Random.Range(0.95f, 1.05f);
+
+        switch(soundName) {
+            case "movement":
+                source.clip = movement;
+                source.Play();
+                break;
+            default:
+                Debug.LogError("Unkown sound to play");
+                break;
+        }
+    }
+    
+}
