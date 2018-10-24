f8fbf65c4b45ac0f1ed85613894a2b70a3f71ed4
Gonzalo Uribe <guribemontero@gmail.com>
2018-10-24 11:49:04 -0300

    Buttons vibrate on touch

---
+++ Assets/Sokoban/Controller/SimpleVibration.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleVibration : MonoBehaviour {
+
+	public void Vibrate() {
+        Vibration.Vibrate(55);
+    }
+}
