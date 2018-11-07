705594dba3f0ea4221dca4f7c0f40d23a643a77c
Gonzalo Uribe <guribemontero@gmail.com>
2018-11-07 12:37:16 -0300

    Testing ui ideas

--- Assets/Sokoban/MainMenu/TransitionManager.cs
+++ Assets/Sokoban/MainMenu/TransitionManager.cs
@@ -1,33 +1,88 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TransitionManager : MonoBehaviour {
 
     [SerializeField] private Image transitionImage;
-    [SerializeField] private float transitionTime;
+    [SerializeField] private float fadeOutTime;
+    [SerializeField] private float fadeInTime;
+    [SerializeField] private GameObject startingScreen;
 
-    public void Transition() {
-        StartCoroutine(FadeOut());
+    private GameObject currentScreen;
+    private Stack<GameObject> previousScreens;
+
+    public void Awake() {
+        currentScreen = startingScreen;
+        previousScreens = new Stack<GameObject>();
     }
 
-    public IEnumerator FadeOut() {
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (previousScreens.Count == 0) {
+                Debug.Log("Quiting");
+                Application.Quit();
+            }
+
+            UndoTransition();
+        }
+    }
+
+    public void Transition(GameObject newScreen) {
+        StartCoroutine(FadeOut(newScreen, true));
+    }
+
+    public void UndoTransition() {
+        StartCoroutine(FadeOut(previousScreens.Pop(), false));
+    }
+
+    public IEnumerator FadeOut(GameObject newScreen, bool addToTransitionStack) {
 
         transitionImage.gameObject.SetActive(true);
         float elapsedTime = 0f;
 
-        while (elapsedTime < transitionTime) {
+        while (elapsedTime < fadeOutTime) {
             elapsedTime += Time.deltaTime;
 
             Color originalColor = transitionImage.color;
-            transitionImage.color = new Color(originalColor.r, originalColor.g, originalColor.b, Mathf.InverseLerp(0f, transitionTime, elapsedTime));
+            transitionImage.color = new Color(originalColor.r, originalColor.g, originalColor.b, Mathf.InverseLerp(0f, fadeOutTime, elapsedTime));
 
             yield return null;
         }
 
         transitionImage.color = new Color(transitionImage.color.r, transitionImage.color.g, transitionImage.color.b, 1f);
-        yield return null;
 
+        currentScreen.SetActive(false);
+        newScreen.SetActive(true);
+
+        if (addToTransitionStack)
+            previousScreens.Push(currentScreen);
+
+        currentScreen = newScreen;
+
+        StartCoroutine(FadeIn());
+        yield return null;
+    }
+
+    public IEnumerator FadeIn() {
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeInTime) {
+            elapsedTime += Time.deltaTime;
+
+            Color originalColor = transitionImage.color;
+            transitionImage.color = new Color(originalColor.r, originalColor.g, originalColor.b, Mathf.InverseLerp(fadeInTime, 0f, elapsedTime));
+
+            yield return null;
+        }
+
+        transitionImage.color = new Color(transitionImage.color.r, transitionImage.color.g, transitionImage.color.b, 0f);
+
+        transitionImage.gameObject.SetActive(false);
+        yield return null;
     }
 	
 }
