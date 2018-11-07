d89b431890ddce706932815a29c6b5b6755037f3
Gonzalo Uribe <guribemontero@gmail.com>
2018-11-07 01:02:53 -0300

    Added transitions

---
+++ Assets/Sokoban/MainMenu/TransitionManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransitionManager : MonoBehaviour {
+
+    [SerializeField] private Image transitionImage;
+    [SerializeField] private float transitionTime;
+
+    public void Transition() {
+        StartCoroutine(FadeOut());
+    }
+
+    public IEnumerator FadeOut() {
+
+        transitionImage.gameObject.SetActive(true);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < transitionTime) {
+            elapsedTime += Time.deltaTime;
+
+            Color originalColor = transitionImage.color;
+            transitionImage.color = new Color(originalColor.r, originalColor.g, originalColor.b, Mathf.InverseLerp(0f, transitionTime, elapsedTime));
+
+            yield return null;
+        }
+
+        transitionImage.color = new Color(transitionImage.color.r, transitionImage.color.g, transitionImage.color.b, 1f);
+        yield return null;
+
+    }
+	
+}
