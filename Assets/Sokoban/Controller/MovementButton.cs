bcfb93f21bff239566d9d196d76edf9c5f0c43c5
Mortup <g.uribemontero@gmail.com>
2018-11-23 13:33:31 -0300

    Added custom movement button script

---
+++ Assets/Sokoban/Controller/MovementButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class MovementButton : Button {
+
+    [SerializeField] private UnityEvent onPressed;
+
+    public override void OnPointerDown(PointerEventData eventData) {
+        base.OnPointerDown(eventData);
+
+        onPressed.Invoke();
+    }
+
+
+}
