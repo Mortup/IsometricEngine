89f995b7feb09a85d04c82044e3a663a4498c44a
Mortup <g.uribemontero@gmail.com>
2019-03-02 17:03:29 -0300

    Adds debug coords text
    A debug text that shows the coordinates the mouse is currently at

---
+++ Assets/CoreEngine/Controller/DebugInfoController.cs
@@ -0,0 +1,43 @@
+﻿using com.gStudios.isometric.controller.isometricTransform;
+using com.gStudios.isometric.model.world;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.gStudios.isometric.controller {
+
+    class DebugInfoController : MonoBehaviour, ILevelController {
+        [SerializeField] Text debugTextField;
+        [Header("Settings")]
+        [SerializeField] bool showMouseCoordinates;
+
+        CoreLevelController clc;
+        Level level;
+
+        public void Init(CoreLevelController clc) {
+            this.clc = clc;
+        }
+
+        public void OnLevelInit(Level level) {
+            this.level = level;
+        }
+
+        public void Update() {
+            if (showMouseCoordinates) {
+                Vector2Int tileCoords = TileTransformer.ScreenToCoord(Input.mousePosition);
+                Vector3Int wallCoords = WallTransformer.ScreenToCoord(Input.mousePosition);
+                debugTextField.text = string.Format(
+                    "Tile Coord: {0}, {1}\nWall Coord: {2}, {3}, {4}",
+                    tileCoords.x,
+                    tileCoords.y,
+                    wallCoords.x,
+                    wallCoords.y,
+                    wallCoords.z);
+            }
+            else {
+                debugTextField.text = "";
+            }
+        }
+
+    }
+
+}
